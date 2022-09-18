843c640fbf107eda58896418b184ea36910931ab
Md. Al-Sajiduzzaman Akand <alsajiduzzaman@outlook.com>
2022-09-18 22:51:54 +0600

    updated

--- src/Pages/register/Registration.js
+++ src/Pages/register/Registration.js
@@ -41,9 +41,16 @@ const Registration = () => {
 
   return (
     <div className="flex h-screen bg-slate-100">
-      <div className="m-auto bg-white shadow-lg p-10 w-3/4 h-5/6">
+      <div className="m-auto bg-white shadow-lg p-10 container ">
+        <div className="border-dotted border-4 border-black h-full py-5 px-5 relative md:hidden">
+          <h2 className="text-lg text-center">দুঃখিত</h2>
+          <p className="leading-7 text-justify">      
+            <br /> নিবন্ধিত তথ্য সংশোধনযোগ্য নয়। অত্যন্ত সতর্কতার সাথে তথ্য পূরণ
+            আবশ্যক। এজন্য মুঠোফোন থেকে নিবন্ধন কার্যক্রম পরিচালনা গ্রহণযোগ্য নয়।
+          </p>
+        </div>
         {loader === "" ? (
-          <div className="border-dotted border-4 border-red-500 h-full py-5 px-5 relative">
+          <div className="border-dotted border-4 border-red-500 h-full py-5 px-5 relative hidden md:block">
             <h1 className="text-3xl text-center sj my-3 text-red-600">
               ডিজিটাল নিকাহ্‌নামা
             </h1>
